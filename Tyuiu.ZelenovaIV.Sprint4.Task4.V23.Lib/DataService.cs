393186df0ee12c6117c752a26abcd2e9d5264f7f
zelenovaIulia <zelenova_yulya@internet.ru>
2025-12-10 06:59:00 +0500

    Task4

---
+++ Tyuiu.ZelenovaIV.Sprint4.Task4.V23.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ZelenovaIV.Sprint4.Task4.V23.Lib
+{
+    public class DataService : ISprint4Task4V23
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] % 2 != 0)
+                    {
+                        sum += matrix[i, j];
+                    }
+                }
+            }
+
+            return sum;
+        }
+    }
+
+}
---
+++ Tyuiu.ZelenovaIV.Sprint4.Task4.V23.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ZelenovaIV.Sprint4.Task4.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
