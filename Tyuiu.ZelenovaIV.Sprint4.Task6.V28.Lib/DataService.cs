f48a6106e0339ccf5859ec2b2a1f3371542e94a5
zelenovaIulia <zelenova_yulya@internet.ru>
2025-12-10 09:22:58 +0500

    Task6

---
+++ Tyuiu.ZelenovaIV.Sprint4.Task6.V28.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ZelenovaIV.Sprint4.Task6.V28.Lib
+{
+    public class DataService : ISprint4Task6V28
+    {
+        public string[] Calculate(string[] array)
+        {
+            return Array.FindAll(array, element => element != null && element.Length == 4);
+        }
+    }
+}
---
+++ Tyuiu.ZelenovaIV.Sprint4.Task6.V28.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ZelenovaIV.Sprint4.Task6.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
