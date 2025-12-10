bd00dfdc2b1675c4fc7738fd93cebfb392911457
zelenovaIulia <zelenova_yulya@internet.ru>
2025-12-10 05:23:10 +0500

    Task0

---
+++ Tyuiu.ZelenovaIV.Sprint4.Task0.V9.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ZelenovaIV.Sprint4.Task0.V9.Lib
+{
+    public class DataService : ISprint4Task0V9
+    {
+        public int GetSumEvenArrEl(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    sum += array[i];
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.ZelenovaIV.Sprint4.Task0.V9.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ZelenovaIV.Sprint4.Task0.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
