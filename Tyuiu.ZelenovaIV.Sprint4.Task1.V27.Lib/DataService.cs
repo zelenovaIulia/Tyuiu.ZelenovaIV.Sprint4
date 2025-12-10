1919d3e762b46fc239060d57ba7f4b7769be4bb4
zelenovaIulia <zelenova_yulya@internet.ru>
2025-12-10 05:40:20 +0500

    Task 1

---
+++ Tyuiu.ZelenovaIV.Sprint4.Task1.V27.Lib/DataService.cs
@@ -0,0 +1,9 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ZelenovaIV.Sprint4.Task1.V27.Lib
+{
+    public class DataService : ISprint4Task1V27
+    {
+        public int Calculate(int[] array) => array.Where(x => x % 2 == 0).Aggregate(1, (acc, x) => acc * x);
+    }
+}
---
+++ Tyuiu.ZelenovaIV.Sprint4.Task1.V27.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ZelenovaIV.Sprint4.Task1.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
