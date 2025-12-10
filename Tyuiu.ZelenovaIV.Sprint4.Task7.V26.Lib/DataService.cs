65d76b902a6af4cf5b4b39ef7d51ac1eb6825e5e
zelenovaIulia <zelenova_yulya@internet.ru>
2025-12-10 11:01:22 +0500

    Task7

---
+++ Tyuiu.ZelenovaIV.Sprint4.Task7.V26.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ZelenovaIV.Sprint4.Task7.V26.Lib
+{
+    public class DataService : ISprint4Task7V26
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            int[,] mtrx = new int[n, m];
+            int index = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    mtrx[i, j] = int.Parse(value.Substring(i * m + j, 1));
+                }
+            }
+            int s = 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (mtrx[i, j] % 2 == 0)
+                    { s = s * mtrx[i, j]; }
+                }
+            }
+            return s;
+        }
+    }
+
+}
---
+++ Tyuiu.ZelenovaIV.Sprint4.Task7.V26.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ZelenovaIV.Sprint4.Task7.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
